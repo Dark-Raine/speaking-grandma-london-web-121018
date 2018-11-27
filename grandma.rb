e48b2b6a2afebd0fb2285a8d58949a3de73f587b
alpha-constant-7616 <anderson-tomas@hotmail.com>
2018-11-27 23:14:11 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,13 @@ def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
-
+    elsif phrase == "WHAT DID YOU EAT TODAY?"
+      return "NO, NOT SINCE 1938!"
     end
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
+
+  
+  
